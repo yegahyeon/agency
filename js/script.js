393081a8eed9c9b03df49a8c335d367077d498fc
예가현 <u1mu2@kakao.com>
2025-01-16 13:13:11 +0900

    반응형 스와이퍼 완성

---
+++ js/script.js
@@ -0,0 +1,25 @@
+AOS.init();
+
+const teamSlider = new Swiper(".team-slider", {
+    loop: true,
+    // autoplay : true,
+    autoplay: {
+        delay: 3000, // 1000 -> 1초
+    },
+    speed: 500,
+
+    //모바일의 세팅으로 시작
+    slidesPerView: 1.5,
+
+    // 반응형 분기점
+    breakpoints: {
+        // 500px 이상
+        500: {
+            slidesPerView: 2.5,
+        },
+        // 1024px 이상
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
